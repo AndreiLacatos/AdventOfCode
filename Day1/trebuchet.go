2c034ef4130e939e6b3fba7116eb2fefd46cd52d
AndreiLacatos <lacatos_andrei@yahoo.com>
2023-12-02 13:00:18 +0200

    Day 1 Part 2

--- Day1/trebuchet.go
+++ Day1/trebuchet.go
@@ -19,8 +19,15 @@ func main() {
 
 	sum := 0
 	for line, err := inputReader.ReadString('\n'); err == nil; line, err = inputReader.ReadString('\n') {
+		print(line)
 		digits := extractDigits(line)
+
+		for _, num := range digits {
+			fmt.Printf("%d ", num)
+		}
+
 		num := makeNumber(digits)
+		println(num)
 		sum += num
 	}
 
@@ -29,13 +36,47 @@ func main() {
 
 func extractDigits(s string) []int {
 	results := []int{}
+	spelled_digits := []string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+
+	buffer := ""
 	for _, char := range s {
 		if char >= '0' && char <= '9' {
 			results = append(results, int(char-'0'))
+			buffer = ""
+		} else {
+			buffer = buffer + string(char)
+			substrings := generateSubstrings(buffer)
+			for _, s := range substrings {
+				index := indexOf(spelled_digits, s)
+				if index > -1 {
+					results = append(results, index+1)
+					buffer = buffer[len(buffer)-1:]
+					break
+				}
+			}
 		}
 	}
 
 	return results
+}
+
+func indexOf(items []string, item string) int {
+	for index, s := range items {
+		if s == item {
+			return index
+		}
+	}
+	return -1
 }
 
 func makeNumber(digits []int) int {
@@ -44,4 +85,15 @@ func makeNumber(digits []int) int {
 	}
 
 	return digits[0]*10 + digits[len(digits)-1]
+}
+
+func generateSubstrings(input string) []string {
+	var result []string
+
+	for i := 0; i < len(input); i++ {
+		substring := input[i:]
+		result = append(result, substring)
+	}
+
+	return result
 }
